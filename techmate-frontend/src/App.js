d39acdf936014ea9502a2ac2d54d805fc7f66f71
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2023-01-09 17:20:07 +0530

    setup routes

--- techmate-frontend/src/App.js
+++ techmate-frontend/src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from './components/Home';
 
 function App() {
-  return  <div> HI  </div>
+
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+      </Routes>
+    </Router>
+    
+
+  )
 }
 
 export default App;
