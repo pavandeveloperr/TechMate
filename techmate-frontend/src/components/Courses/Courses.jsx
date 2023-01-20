059e0e5ec51181247b7b8f93d1611b58db6049ef
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2023-01-20 14:41:33 +0530

    Revert "feat: seach and categories component"
    This reverts commit 45354ec5eb4e80965e15fbf6ca9a84ce9ee6708e.

--- techmate-frontend/src/components/Courses/Courses.jsx
+++ techmate-frontend/src/components/Courses/Courses.jsx
@@ -1,33 +1,9 @@
-import React from 'react';
-import { useState } from 'react';
-
-// ChakraUI components
-import { Container, Heading, Input, Text, HStack, Button } from '@chakra-ui/react';
+import React from 'react'
 
 const Courses = () => {
-  const [keyword, setKeyword] = useState('');
-
   return (
-    <Container minH={'95vh'} maxW="container.lg" paddingY={'8'}>
-      <Heading children="All Courses" m={'8'} />
+    <div>courses</div>
+  )
+}
 
-      {/* search input */}
-      <Input
-        value={keyword}
-        onChange={e => setKeyword(e.target.value)}
-        placeholder="Search a course..."
-        type={'text'}
-        focusBorderColor="yellow.500"
-      />
-
-      {/* categories button */}
-      <HStack>
-        <Button minW={'60'}>
-          <Text children="Category1" />
-        </Button>
-      </HStack>
-    </Container>
-  );
-};
-
-export default Courses;
+export default Courses
