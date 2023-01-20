45863c7a2bdab5c68f64e097248585731e096886
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2023-01-20 14:42:50 +0530

    feat : search and categories component

--- techmate-frontend/src/components/Courses/Courses.jsx
+++ techmate-frontend/src/components/Courses/Courses.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+
+// ChakraUI components
+import { Container, Heading, Input, Text, HStack, Button } from '@chakra-ui/react';
 
 const Courses = () => {
-  return (
-    <div>courses</div>
-  )
-}
+  const [keyword, setKeyword] = useState('');
 
-export default Courses
+  return (
+    <Container minH={'95vh'} maxW="container.lg" paddingY={'8'}>
+      <Heading children="All Courses" m={'8'} />
+
+      {/* search input */}
+      <Input
+        value={keyword}
+        onChange={e => setKeyword(e.target.value)}
+        placeholder="Search a course..."
+        type={'text'}
+        focusBorderColor="yellow.500"
+      />
+
+      {/* categories button */}
+      <HStack>
+        <Button minW={'60'}>
+          <Text children="Category1" />
+        </Button>
+      </HStack>
+    </Container>
+  );
+};
+
+export default Courses;
