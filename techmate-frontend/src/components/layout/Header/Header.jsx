c9ec88b738eef322d36b25a8bc8ac0aef7bc3d1f
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2023-01-20 14:08:19 +0530

    perf : clean code

--- techmate-frontend/src/components/layout/Header/Header.jsx
+++ techmate-frontend/src/components/layout/Header/Header.jsx
@@ -1,113 +1,122 @@
-import React from 'react'
+import React from 'react';
 import { ColorModeSwitcher } from '../../../ColorModeSwitcher';
-import { Button, Drawer, DrawerBody, DrawerContent, DrawerHeader, DrawerOverlay, HStack, useDisclosure, VStack } from '@chakra-ui/react';
-import { RiDashboardFill, RiLogoutBoxLine, RiMenu5Fill } from 'react-icons/ri'
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  HStack,
+  useDisclosure,
+  VStack,
+} from '@chakra-ui/react';
+import { RiDashboardFill, RiLogoutBoxLine, RiMenu5Fill } from 'react-icons/ri';
 import { Link } from 'react-router-dom';
 
 // Nav menu buttons component
-const LinkButton = ({ url = "/", title = "Home" }) =>
-(
-    <Link>
-        <Button variant={'ghost'}>{title}</Button>
-    </Link>
-)
+const LinkButton = ({ url = '/', title = 'Home', onClose }) => (
+  <Link onClick={onClose} to={url}>
+    <Button variant={'ghost'}>{title}</Button>
+  </Link>
+);
 
-// authentication variables
-const isAuthenticated = true;
-const user = {
-    role: 'admin',
-}
-
-const logoutHandler = () => {
-    console.log("Logout");
-}
 
 const Header = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    return (
-        <>
-            <ColorModeSwitcher />
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-            <Button colorScheme={'yellow'}
-                onClick={onOpen}
-                width="12"
-                height={'12'}
-                rounded="full"
-                position={"fixed"}
-                top='6'
-                left='6'
-            >
-                <RiMenu5Fill />
-            </Button>
+  
+    // authentication variables
+    const isAuthenticated = true;
+    const user = {
+        role: 'admin',
+    };
 
-            <Drawer placement='left' isOpen={isOpen} onClose={onClose}>
-                <DrawerOverlay />
-                <DrawerContent>
-                    <DrawerHeader borderBottomWidth={"2px"}>Tech Mate</DrawerHeader>
-                    <DrawerBody>
-                        <VStack spacing={'4'} alignItems="flex-start">
-                            <LinkButton url="/" title="Home" />
-                            <LinkButton url="/courses" title="Browse All Courses" />
-                            <LinkButton url="/request" title="Request a Course" />
-                            <LinkButton url="/contact" title="Contact Us" />
-                            <LinkButton url="/about" title="About" />
+    const logoutHandler = () => {
+        console.log('Logout');
+        onClose();
+    };
 
-                            <HStack
-                                justifyContent={'space-evenly'}
-                                position='absolute'
-                                bottom={'2rem'}
-                                width='80%'>
-                                {isAuthenticated ? (
-                                    <>
-                                    <VStack> 
-                                        <HStack>
-                                            <Link to={"/profile"} >
-                                                <Button
-                                                    colorScheme={'yellow'}
-                                                    variant={'ghost'}>
-                                                    Profile
-                                                </Button>
-                                            </Link>
-                                            <Button
-                                                variant={'ghost'}
-                                                onClick={logoutHandler}>
-                                                <RiLogoutBoxLine />
-                                                Logout
-                                            </Button>
-                                        </HStack>
+  return (
+    <>
+      <ColorModeSwitcher />
 
-                                        {
-                                            user && user.role ==="admin" && 
-                                            <Link to="/admin/dashboard">
-                                                <Button colorScheme={'purple'} variant="ghost">
-                                                    <RiDashboardFill style={{margin: '2px'}}/>
-                                                    Dashboard
-                                                </Button>
-                                            </Link>
-                                        }
-                                        </VStack>
-                                    </>
-                                )
-                                    : (
-                                        <>
-                                            <Link to={"/login"} >
-                                                <Button colorScheme={'yellow'}>login</Button>
-                                            </Link>
+      <Button
+        colorScheme={'yellow'}
+        onClick={onOpen}
+        width="12"
+        height={'12'}
+        rounded="full"
+        position={'fixed'}
+        top="6"
+        left="6"
+      >
+        <RiMenu5Fill />
+      </Button>
 
-                                            <p>OR</p>
+      <Drawer placement="left" isOpen={isOpen} onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth={'2px'}>Tech Mate</DrawerHeader>
+          <DrawerBody>
+            <VStack spacing={'4'} alignItems="flex-start">
+              <LinkButton onClose={onClose} url="/" title="Home" />
+              <LinkButton onClose={onClose} url="/courses" title="Browse All Courses" />
+              <LinkButton onClose={onClose} url="/request" title="Request a Course" />
+              <LinkButton onClose={onClose} url="/contact" title="Contact Us" />
+              <LinkButton onClose={onClose} url="/about" title="About" />
 
-                                            <Link to={"/register"} >
-                                                <Button colorScheme={'yellow'}>Sign Up</Button>
-                                            </Link>
-                                        </>
-                                    )}
-                            </HStack>
-                        </VStack>
-                    </DrawerBody>
-                </DrawerContent>
-            </Drawer>
-        </>
-    )
-}
+              <HStack
+                justifyContent={'space-evenly'}
+                position="absolute"
+                bottom={'2rem'}
+                width="80%"
+              >
+                {isAuthenticated ? (
+                  <>
+                    <VStack>
+                      <HStack>
+                        <Link onClick={onClose} to={'/profile'}>
+                          <Button colorScheme={'yellow'} variant={'ghost'}>
+                            Profile
+                          </Button>
+                        </Link>
+                        <Button variant={'ghost'} onClick={logoutHandler}>
+                          <RiLogoutBoxLine />
+                          Logout
+                        </Button>
+                      </HStack>
 
-export default Header
+                      {user && user.role === 'admin' && (
+                        <Link onClick={onClose} to="/admin/dashboard">
+                          <Button colorScheme={'purple'} variant="ghost">
+                            <RiDashboardFill style={{ margin: '2px' }} />
+                            Dashboard
+                          </Button>
+                        </Link>
+                      )}
+                    </VStack>
+                  </>
+                ) : (
+                  <>
+                    <Link  onClick={onClose} to={'/login'}>
+                      <Button colorScheme={'yellow'}>login</Button>
+                    </Link>
+
+                    <p>OR</p>
+
+                    <Link onClick={onClose} to={'/register'}>
+                      <Button colorScheme={'yellow'}>Sign Up</Button>
+                    </Link>
+                  </>
+                )}
+              </HStack>
+            </VStack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
+
+export default Header;
