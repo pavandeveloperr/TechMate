6d04c715460c7a75f7413f9ea63c3228ee75a028
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2023-01-22 09:47:02 +0530

    feat : course card component

--- techmate-frontend/src/components/Courses/Courses.jsx
+++ techmate-frontend/src/components/Courses/Courses.jsx
@@ -2,10 +2,105 @@ import React from 'react';
 import { useState } from 'react';
 
 // ChakraUI components
-import { Container, Heading, Input, Text, HStack, Button } from '@chakra-ui/react';
+import {
+  Container,
+  Heading,
+  Input,
+  Text,
+  HStack,
+  Button,
+  GridItem,
+  Stack,
+  VStack,
+  Image,
+  AlertDescription,
+  Link,
+} from '@chakra-ui/react';
+
+const Course = ({
+  views,
+  title,
+  ImageSrc,
+  id,
+  addToPlaylistHandler,
+  creator,
+  discription,
+  lecture,
+  lectureCount,
+}) => {
+  return (
+    <VStack className="course" alignItems={['center', 'flex-start']}>
+      <Image src={ImageSrc} boxSize="60" objectFit={'contain'} marginY="7" />
+      <Heading
+        textAlign={['ceneter', 'left']}
+        maxW="200px"
+        size={'sm'}
+        fontFamily={'sans-serif'}
+        noOfLines={3}
+        children={title}
+      />
+
+      <Text children={discription} />
+
+      <HStack>
+        <Text
+          fontWeight={'bold'}
+          textTransform="uppercase"
+          children={creator}
+        />
+
+        <Text
+          fontWeight={'body'}
+          textTransform="uppercase"
+          children={creator}
+        />
+      </HStack>
+
+      <Heading
+        textAlign={'center'}
+        size="xs"
+        children={`Lectures - ${lectureCount}`}
+        textTransform="uppercase"
+      />
+
+      <Heading
+        size="xs"
+        children={`Views - ${views}`}
+        textTransform="uppercase"
+      />
+
+      <Stack direction={['column', 'row']} alignItems="center">
+        <Link to={`/courses/${id}`}>
+          <Button colorScheme={'yellow'}>Watch Now</Button>
+        </Link>
+        <Button
+          // isLoading={loading}
+          variant={'ghost'}
+          colorScheme={'yellow'}
+          onClick={() => addToPlaylistHandler(id)}
+        >
+          Add to playlist
+        </Button>
+      </Stack>
+    </VStack>
+  );
+};
 
 const Courses = () => {
   const [keyword, setKeyword] = useState('');
+  const [category, setCategory] = useState('');
+
+  const addToPlaylistHandler = () => {
+    console.log("Added to Playlist")
+  }
+
+  const categories = [
+    'Web Development',
+    'Artificial Intelligence',
+    'Data Structures & Algorithms',
+    'App Development',
+    'Game Development',
+  ];
 
   return (
     <Container minH={'95vh'} maxW="container.lg" paddingY={'8'}>
@@ -21,11 +116,45 @@ const Courses = () => {
       />
 
       {/* categories button */}
-      <HStack>
-        <Button minW={'60'}>
-          <Text children="Category1" />
-        </Button>
+      <HStack
+        overflowX={'auto'}
+        paddingY="8"
+        css={{
+          '&::-webkit-scrollbar': {
+            width: '4px',
+          },
+          '&::-webkit-scrollbar-track': {
+            width: '4px',
+          },
+          '&::-webkit-scrollbar-thumb': {
+            background: '#D3D3D3',
+            borderRadius: '24px',
+          },
+        }}
+      >
+        {categories.map((item, index) => (
+          <Button key={index} onClick={() => setCategory(item)} minW={'60'}>
+            <Text children={item} />
+          </Button>
+        ))}
       </HStack>
+      <Stack
+        direction={['column', 'row']}
+        flexWrap="wrap"
+        justifyContent={['flex-start', 'space-evenly']}
+        alignItems={['center', 'flex-start']}
+      >
+        <Course
+          title={'Sample'}
+          discription={'sample'}
+          views={23}
+          imageSrc={'https://pixabay.com/images/id-1076536/'}
+          id={'sample'}
+          creator={'sample'}
+          lectureCount={4}
+          addToPlaylistHandler = {addToPlaylistHandler}
+        />
+      </Stack>
     </Container>
   );
 };
