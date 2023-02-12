bd005c99427fa2a1616e5b030bb8e7d198991758
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2023-02-12 19:00:20 +0530

    update routes

--- techmate-frontend/src/App.js
+++ techmate-frontend/src/App.js
@@ -11,6 +11,10 @@ import ResetPassword from './components/Auth/ResetPassword';
 import Contact from './components/Contact/Contact';
 import Request from './components/Request/Request';
 import About from './components/About/About';
+import Subscribe from './components/Payments/Subscribe';
+import NotFound from './components/NotFound/NotFound';
+import PaymentSuccess from './components/Payments/PaymentSuccess';
+import PaymentFail from './components/Payments/PaymentFail';
 
 function App() {
 
@@ -28,6 +32,10 @@ function App() {
         <Route path='/forgetpassword' element={<ForgetPassword />}/>
 
         <Route path='/resetpassword/:token' element={<ResetPassword />}/>
+        <Route path='/subscribe' element={<Subscribe />}/>
+        <Route path='/*' element={<NotFound />}/>
+        <Route path='/paymentsuccess' element={<PaymentSuccess />}/>
+        <Route path='/paymentfail' element={<PaymentFail />}/>
       </Routes>
         <Footer />
     </Router>
