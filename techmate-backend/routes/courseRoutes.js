5483ab7a5eb9990cc780d3e00a8bd264ed3e770c
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2023-05-21 01:11:29 +0530

    api : course create endpoint✔

--- techmate-backend/routes/courseRoutes.js
+++ techmate-backend/routes/courseRoutes.js
@@ -1,8 +1,19 @@
 import express from "express";
-import { getAllCourses } from "../controllers/courseController.js";
+import {
+  createCourse,
+  getAllCourses,
+} from "../controllers/courseController.js";
 
 const router = express.Router();
 
+// Get All courses without lectures
 router.route("/courses").get(getAllCourses);
+
+// create new course - only admin
+router.route("/createcourse").post(createCourse);
+
+// Add lecture, delete course, get course details
+
+// Delete Lecture
 
 export default router;
