78633ada98adebcbc65a2cf552269f1db0631fd4
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2023-05-21 01:10:31 +0530

    reset password & profile updation

--- techmate-backend/controllers/userController.js
+++ techmate-backend/controllers/userController.js
@@ -2,6 +2,8 @@ import { catchAsyncError } from "../middlewares/catchAsyncError.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import { User } from "../models/User.js";
 import { sendToken } from "../utils/sendToken.js";
+import { sendEmail } from "../utils/sendEmail.js";
+import crypto from "crypto";
 
 export const register = async (req, res, next) => {
   try {
@@ -40,7 +42,7 @@ export const login = catchAsyncError(async (req, res, next) => {
   // const file = req.file;
 
   if (!email || !password)
-    return next(new ErrorHandler("Please enter all fields", 420));
+    return next(new ErrorHandler("Please enter all fields", 400));
 
   const user = await User.findOne({ email }).select("+password");
 
@@ -64,4 +66,115 @@ export const logout = catchAsyncError(async (req, res, next) => {
       success: true,
       message: "Logged Out Successfully",
     });
+});
+
+export const getMyProfile = catchAsyncError(async (req, res, next) => {
+  const user = await User.findById(req.user._id);
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
+
+export const changePassword = catchAsyncError(async (req, res, next) => {
+  const { oldPassword, newPassword } = req.body;
+
+  if (!oldPassword || !newPassword)
+    return next(new ErrorHandler("Please enter all fields", 400));
+
+  const user = await User.findById(req.user._id).select("+password");
+
+  const isMatch = await user.comparePassword(oldPassword);
+
+  if (!isMatch) return next(new ErrorHandler("Incorrect Old Password", 400));
+
+  user.password = newPassword;
+
+  await user.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Password Changed Successfully",
+  });
+});
+
+export const updateProfile = catchAsyncError(async (req, res, next) => {
+  const { name, email } = req.body;
+
+  const user = await User.findById(req.user._id).select("+password");
+
+  if (name) user.name = name;
+  if (email) user.email = email;
+
+  await user.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Profile Updated Successfully",
+  });
+});
+
+export const updateProfilePicture = catchAsyncError(async (req, res, next) => {
+  // ADD Cloudinary - TODO
+  res.status(200).json({
+    success: true,
+    message: "Profile Picture Updated Successfully",
+  });
+});
+
+export const forgetPassword = catchAsyncError(async (req, res, next) => {
+  const { email } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user)
+    return next(
+      new ErrorHandler("No User Exists with this Email Address!", 400)
+    );
+
+  const resetToken = await user.getResetToken();
+
+  await user.save();
+
+  // send token via email
+
+  const url = `${process.env.FRONTEND_URL}/resetpassword/${resetToken}`;
+
+  const message = `Click on the link to reset your password. ${url}. If you have not requested then please ignore.`;
+  sendEmail(user.email, "Techmate Reset Password", message);
+
+  res.status(200).json({
+    success: true,
+    message: `Reset Token has been sent to ${user.email}`,
+  });
+});
+
+export const resetPassword = catchAsyncError(async (req, res, next) => {
+  const { token } = req.params;
+
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(token)
+    .digest("hex");
+
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: {
+      $gt: Date.now(),
+    },
+  });
+  if (!user)
+    return next(new ErrorHandler("Invalid Token or has been expired", 401));
+
+  user.password = req.body.password;
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
+
+  await user.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Password Changed Successfully",
+    token,
+  });
 });
