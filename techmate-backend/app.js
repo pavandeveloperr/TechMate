6d0e3d580a425cb146ba3d8f4f476ac89e8327a9
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2023-05-20 18:34:57 +0530

    added morgan dependency

--- techmate-backend/app.js
+++ techmate-backend/app.js
@@ -1,11 +1,22 @@
 import express from "express";
 import { config } from "dotenv";
+import morgan from "morgan";
+import ErrorMiddleware from "./middlewares/Error.js";
 
 config({
   path: "./config/config.env",
 });
 
 const app = express();
+
+// using middlewares
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(morgan("dev"));
 
 // Importing & Using routes
 import course from "./routes/courseRoutes.js";
@@ -15,3 +26,5 @@ app.use("/api/v1", course);
 app.use("/api/v1", user);
 
 export default app;
+
+app.use(ErrorMiddleware);
